8d974b62e3ec9cc433d1b362265998cb94be1c9e
yuxin <yuxin@wacai.com>
2016-06-27 15:40:37 +0800

    add BubbleSort

---
+++ src/com/base/algorithm/BubbleSort.java
@@ -0,0 +1,44 @@
+package com.base.algorithm;
+
+import java.util.Iterator;
+
+/**
+ * Created by john on 2016/6/27.
+ */
+public class BubbleSort<T extends  Comparable<T>>{
+
+    private BubbleSort(){}
+
+    public static <T extends Comparable<T>> T[] sort(T[] unsorted){
+        boolean swapped = true;
+        int length = unsorted.length;
+        while(swapped){
+            swapped = false;
+            for(int i = 1; i < length; i++){
+                if(unsorted[i].compareTo(unsorted[i - 1]) < 0){
+                    swap(i, i - 1, unsorted);
+                    swapped = true;
+                }
+            }
+            length--;
+        }
+        return unsorted;
+    }
+
+
+    public static <T extends Comparable<T>>  void swap(int index1, int index2, T[] unsorted){
+        T value = unsorted[index1];
+        unsorted[index1] = unsorted[index2];
+        unsorted[index2] = value;
+    }
+
+    public static void main(String[] args){
+        Integer[] a = new Integer[]{2,12,3,8,1,79,5};
+        a = BubbleSort.sort(a.clone());
+        for(int i = 0; i < a.length; i++){
+            System.out.println(String.valueOf(a[i]));
+        }
+    }
+
+
+}
--- src/com/base/algorithm/bubboSort.java
+++
@@ -1,7 +0,0 @@
-package com.base.algorithm;
-
-/**
- * Created by john on 2016/6/27.
- */
-public class bubboSort {
-}
